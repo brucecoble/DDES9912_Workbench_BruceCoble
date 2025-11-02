b629a3967eb80f645b2ded4eefd52c2478a08557
brucecoble <bcoble7@gmail.com>
2025-11-02 20:25:03 +1100

    Adding Terrain Assets

---
+++ Assets/__My DDES9912 Work/Week 7/Scripts/GhostWalk.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GhostWalk : MonoBehaviour
+{
+    [Header("Chase Settiings")]
+    public Transform chaseTarget;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
