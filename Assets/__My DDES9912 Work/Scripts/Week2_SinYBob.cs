e4f6e832ba2f7ced7ecc9ea37cac61166c100be8
Bruce Coble <bcoble7@gmail.com>
2025-11-20 01:47:04 +1100

    Adding Week 2 Files

---
+++ Assets/__My DDES9912 Work/Scripts/Week2_SinYBob.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Week2_SinYBob : MonoBehaviour
+{
+    public Vector3 startPosition; 
+    public Vector3 sinOffset;
+    public float alpha;
+    public float sinValue;
+    public float rangeFactor;
+    public float bobSpeed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sinValue = Mathf.Sin(alpha * Mathf.Deg2Rad);
+        
+        sinOffset.y = sinValue * rangeFactor;
+
+        transform.localPosition = startPosition + sinOffset;
+
+        alpha += bobSpeed * Time.deltaTime;
+
+    }
+}
+    
